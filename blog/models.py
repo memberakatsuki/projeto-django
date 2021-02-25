16c0c730a27665d144f6fd3f1edcfe3ca96b590f
Allyson <allysonsousaplay123@gmail.com>
2021-02-25 07:54:44 -0300

    Criação do modelo Post

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+	STATUS = (
+		('draft', 'Draft'),
+		('published', 'Published'),
+	)
+	title = models.CharField(max_length=250)
+	slug  = models.SlugField(max_length=250)
+	auth  = models.ForeignKey(User,
+							on_delete=models.CASCADE)
+
+	content   = models.TextField()
+	published = models.DateTimeField(default=timezone.now)
+	created   = models.DateTimeField(auto_now_add=True)
+	changed   = models.DateTimeField(auto_now=True)
+	status    = models.CharField(max_length=10,
+								choices=STATUS,
+								default='draft')
+
+	class Meta:
+		ordering = ('-published',)
+
+	def __str__(self):
+		return self.title
+
+# Create your models here.
